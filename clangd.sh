fea31bb978320c64b237e271bd7fcfe5d2e4e93f
Morgan Arrington <mjarring96@gmail.com>
2026-04-30 22:58:01 -0400

    Fix clangd.sh and add .clangd
    Fix clangd.sh to use correct environment vars. Also add .clangd file so
    clangd looks in the right directory.

--- clangd.sh
+++ clangd.sh
@@ -2,6 +2,9 @@
 
 # Generate aarch64 compile_commands.json for clangd
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MYAPP_SRC_DIR=$SCRIPT_DIR/source
+
 source compile-common.sh
 
 # Make sure build dir exists
@@ -15,13 +18,13 @@ cat >compile_commands.json <<EOF
 [
   {
     "directory": "$(pwd)",
-    "command": "gcc ${CFLAGS[*]} ${LDFLAGS[*]} -o myapp $SRC $GEN_C",
-    "file": "$SRC"
+    "command": "gcc ${MYAPP_CFLAGS[*]} ${MYAPP_LDFLAGS[*]} -o myapp $MYAPP_SRC $MYAPP_GEN_C",
+    "file": "$MYAPP_SRC"
   },
   {
     "directory": "$(pwd)",
-    "command": "gcc -I. -c $GEN_C -o xdg-shell-protocol.o",
-    "file": "$GEN_C"
+    "command": "gcc -I. -c $MYAPP_GEN_C -o xdg-shell-protocol.o",
+    "file": "$MYAPP_GEN_C"
   }
 ]
 EOF
